9a46478160b4ffd43a5072f73db6c59bdf305309
Blen23 <blenfesehaye@gmail.com>
2021-07-18 04:53:12 -0700

    add a space before and after the astrix

--- pyramid.js
+++ pyramid.js
@@ -5,9 +5,14 @@
     </head>
     <body>
     <script type= "text/javascript">
-    var i, i; col=5;
+    var i, i, k, col=5;
     for(i=0; i<col; i++)
              {
+                 for(k=0; k<=(col-i); k++)
+                 {
+                     
+                 }
+
                  for(j=0; j<=i; j++)  
                  {
                     document.write("*");
