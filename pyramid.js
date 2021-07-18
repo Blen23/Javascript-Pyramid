8f875ca3961f3871fa2ad23005cff69232d720ae
Blen23 <blenfesehaye@gmail.com>
2021-07-18 04:50:56 -0700

    add a new line to be printed after EVRYTIME THE INNER LOOP finishes excuting

--- pyramid.js
+++ pyramid.js
@@ -5,13 +5,14 @@
     </head>
     <body>
     <script type= "text/javascript">
-    var i, i, col=5;
+    var i, i; col=5;
     for(i=0; i<col; i++)
              {
                  for(j=0; j<=i; j++)  
                  {
                     document.write("*");
                  }
+                 document.write("<br/>");
              }
 
         </script>
